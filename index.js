02b65760478399623d61fd144d692b892210cd52
Marcus Haddon <haddon.marcus@gmail.com>
2018-06-14 19:49:50 -0500

    Open browser and websocketd from node.

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
-function log_(msg) {
-    document.getElementById('log').textContent += msg + '\n';
-}
-function draw() {
-    // setup websocket with callbacks
-    var ws = new WebSocket('ws://localhost:8080/');
-    ws.onopen = function () {
-        log_('CONNECT');
-    };
-    ws.onclose = function () {
-        log_('DISCONNECT');
-    };
-    ws.onmessage = function (event) {
-        log_('MESSAGE: ' + event.data);
-    };
-}
-function setup() {
-    document.querySelector('#trigger')
-        .addEventListener('click', draw);
-}
-document.addEventListener('DOMContentLoaded', setup);
+const { exec } = require('child_process');
+
+exec('google-chrome frontend/index.html', (err, stdout, stderr) => {
+    console.log(stdout);
+
+    exec('websocketd --port=8080 ./bytes.sh', (err, stdout, stderr) => {
+        console.log(stdout);
+    })
+});
+
